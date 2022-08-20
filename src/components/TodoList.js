fd48deb7d2155e24b10e44ce19d145bda73693c2
dipesh shakya <dipeshshakya106@gmail.com>
2022-08-20 12:45:36 +1000

    completed functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { FaTrash, FaPencilAlt } from 'react-icons/fa';
+import { React, useState } from 'react';
+import {
+  FaTrash,
+  FaPencilAlt,
+  FaRegCircle,
+  FaRegCheckCircle,
+} from 'react-icons/fa';
 
 export const TodoList = ({ items, id, deleteItem, editItem }) => {
+  const [isComplete, setIsComplete] = useState(false);
   return (
     <>
       <div
         className="container rounded-sm shadow-sm border-slate-500 p-4 hover:shadow-md mb-4 flex justify-between"
         key={id}
       >
-        <p>{items}</p>
+        <div className="flex items-center justify-evenly">
+          {!isComplete ? (
+            <FaRegCircle onClick={() => setIsComplete(true)} />
+          ) : (
+            <FaRegCheckCircle />
+          )}
+
+          <p
+            style={{
+              textDecoration: isComplete && 'line-through',
+              textDecorationColor: isComplete && 'grey',
+            }}
+          >
+            {items}
+          </p>
+        </div>
         <div className="flex">
           <FaPencilAlt className="mx-1" onClick={() => editItem(id)} />
           <FaTrash className="mx-1" onClick={() => deleteItem(id)} />
